104f354554314bf8a7dbfa4df26cbb39aa036199
jayant242005 <malikwadejay@gmail.com>
2025-12-12 21:38:16 +0530

    question

---
+++ sunbeam/question2.py
@@ -0,0 +1,5 @@
+num1 = input("enter first number:")
+num2 = input("enter second number:")
+
+sum = int(num1) + int(num2)
+print("the sum of two numbers is:",sum)
---
+++ sunbeam/question3.py
@@ -0,0 +1,13 @@
+num1 = input("enter first  number")
+num2 = input("enter second number")
+
+print("\n before swapping")
+print("first number:",num1)
+print("second number:",num2)
+
+temp1 = num1
+num1= num2
+num2= temp1
+print("\n number after swap")
+print("first number:",num1)
+print("second number:",num2)
